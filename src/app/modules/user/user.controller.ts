4409f70eaab6255453086deba9886257a6bde59e
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-07-22 21:56:44 +0600

    catch async function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,20 +1,46 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { userServices } from "./user.service";
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+import { catchAsync } from "../../utils/catchAsync";
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const user = await userServices.createUser(req.body);
+
+//     res.status(httpStatus.CREATED).json({
+//       message: "USer created successfully",
+//       user,
+//     });
+//   } catch (error: any) {
+//     console.log(error);
+//     next(error);
+//   }
+// };
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await userServices.createUser(req.body);
 
     res.status(httpStatus.CREATED).json({
       message: "USer created successfully",
       user,
     });
-  } catch (error: any) {
-    console.log(error);
-    next(error);
   }
-};
+);
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await userServices.getAllUsers();
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "all USer retrieved successfully",
+      users,
+    });
+  }
+);
 export const userControllers = {
   createUser,
+  getAllUsers,
 };
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -3,4 +3,5 @@ import { userControllers } from "./user.controller";
 
 const router = Router();
 router.post("/register", userControllers.createUser);
+router.get("/all-users", userControllers.getAllUsers);
 export const UserRoutes = router;
